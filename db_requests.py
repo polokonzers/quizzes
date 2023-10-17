fde39b3a1db7e035dd44f88a354fc80bc768fc4a
Никита Берлетов <polokonzers@yahoo.com>
2023-10-17 20:44:56 +0300

    Added question changer if question is not unique

--- db_requests.py
+++ db_requests.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+import aiohttp
+from sqlalchemy import exists, create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -27,6 +28,39 @@ def add_to_db(responce_json):
             answer=item['answer'],
             created_at=item['created_at']
         )
-    session.add(quiz_data)
-    session.commit()
+        session.add(quiz_data)
+        session.commit()
+        session.close()
+
+
+async def unique_checker(responce_json, url):
+    '''Check question_id existance'''
+    unique_responce = []
+    session = Session()
+    for item in responce_json:
+        question_id_exists = session.query(
+            exists().where(QuizData.jservice_question_id == item['id'])
+        ).scalar()
+        if question_id_exists:
+            unique_question = await question_changer(url)
+            unique_responce.append(unique_question)
+        else:
+            unique_responce.append(item)
+    return unique_responce
+
+
+async def question_changer(url):
+    params = {'count': 1}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as response:
+            new_response_json = await response.json()
+    session = Session()
+    question_id_exists = session.query(
+        exists().where(
+            QuizData.jservice_question_id == new_response_json[0]['id']
+        )
+    ).scalar()
     session.close()
+    if question_id_exists:
+        return await question_changer(url)
+    return new_response_json[0]
--- main.py
+++ main.py
@@ -16,13 +16,24 @@ app = FastAPI()
 
 @app.post("/")
 async def root(user_quiz_request: UserQuizRequest):
+    '''Handling user workflow'''
     url = JSERVICE_ENPOINT
     params = {
         'count': user_quiz_request.questions_num
     }
+    responce_json = await jservice_request(url, params)
+    # Here we need to check unique id of question
+    db_requests.add_to_db(responce_json)
+    # Here we need to return to user a last question from DB
+    return responce_json
+
+
+async def jservice_request(url, params):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as responce:
             responce_json = await responce.json()
-            db_requests.add_to_db(responce_json)
-            # Here we need to return to user a last question from DB
-            return responce_json
+            validated_responce = await db_requests.unique_checker(
+                responce_json,
+                url
+            )
+            return validated_responce
