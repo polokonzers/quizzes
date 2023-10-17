b6b01d62181447b257dc82e8a463e1f2ebae0b5c
Никита Берлетов <polokonzers@yahoo.com>
2023-10-17 14:28:24 +0300

    Connected to DB

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import aiohttp
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+Base = declarative_base()
 
 JSERVICE_ENPOINT = "https://jservice.io/api/random/"
 
@@ -10,12 +18,22 @@ class UserQuizRequest(BaseModel):
     questions_num: int
 
 
+class QuizData(Base):
+    __tablename__ = 'quiz_data'
+
+    quiz_id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    created_at = Column(DateTime)
+
+
 app = FastAPI()
+engine = create_engine('postgresql://quiz_admin:1234@localhost/quiz')
+Session = sessionmaker(bind=engine)
 
 
 @app.post("/")
 async def root(user_quiz_request: UserQuizRequest):
-    # Here we just return jservice answer
     url = JSERVICE_ENPOINT
     params = {
         'count': user_quiz_request.questions_num
@@ -23,4 +41,16 @@ async def root(user_quiz_request: UserQuizRequest):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as responce:
             responce_json = await responce.json()
+            session = Session()
+            for item in responce_json:
+                quiz_data = QuizData(
+                    quiz_id=item['id'],
+                    question=item['question'],
+                    answer=item['answer'],
+                    created_at=item['created_at']
+                )
+                session.add(quiz_data)
+            session.commit()
+            session.close()
+            # Here we need to return to user a last question from DB
             return responce_json
